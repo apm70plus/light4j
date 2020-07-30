c5438206c6f88bd103da5bab6f4b83940f403c72
daliu <liuyg@aixahc.com>
2020-07-30 20:49:08 +0800

    重构shiro配置，支持hazelcast、redis做为共享存储

--- samples/web-jpa-sample/src/main/java/com/daliu/sample/controller/TestController.java
+++ samples/web-jpa-sample/src/main/java/com/daliu/sample/controller/TestController.java
@@ -1,13 +1,38 @@
 package com.daliu.sample.controller;
 
+import com.daliu.sample.convertor.CourseConvertor;
+import com.daliu.sample.model.Course;
+import com.daliu.sample.repository.CourseRepository;
+import com.daliu.sample.service.CourseService;
+import com.light.exception.BusinessException;
+import com.light.web.response.PageResponse;
+import com.light.web.response.RestResponse;
+import com.querydsl.core.types.Predicate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.light.exception.BusinessException;
-import com.light.web.response.RestResponse;
-
+@Slf4j
 @RestController
 public class TestController {
+
+	@Autowired
+	private CourseRepository courseRepository;
+	@Autowired
+	private CourseConvertor courseConvertor;
+	@Autowired
+	private CourseService courseService;
+
+	@PostMapping("/test")
+	public PageResponse<Course> search(final Pageable pageable, @QuerydslPredicate(root= Course.class) Predicate predicate) {
+		final Page<Course> models = this.courseRepository.findAll(predicate, pageable);
+		return PageResponse.success(models);
+	}
 
 	@GetMapping("/hello")
 	public RestResponse<String> hello() {
